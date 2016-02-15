9520a6e3c60d5d22a7318e3e1e5d9d6546bcde19
Isabelle Rosenquist <isabelle.rosenquist@gmail.com>
2016-02-15 11:41:45 +0100

    Nu funkar det!

--- js/golv.js
+++ js/golv.js
@@ -3,9 +3,21 @@ var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeigh
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement); 
-var geometry = new THREE.BoxGeometry(1, 1, 1); 
+//var geometry = new THREE.BoxGeometry(1,1,1);
+var geometry = new THREE.Geometry(); 
+
+
+geometry.vertices.push(new THREE.Vector3(-0.75, 0, -10));//Vertice 0
+geometry.vertices.push(new THREE.Vector3( 0.75, 0, -10));//Vertice 1
+geometry.vertices.push(new THREE.Vector3( 0.75, 0,  10));//Vertice 2
+geometry.vertices.push(new THREE.Vector3(-0.75, 0,  10));//Vertice 3
+		
+geometry.faces.push(new THREE.Face3( 1, 0, 2));
+geometry.faces.push(new THREE.Face3( 2, 0, 3));
+			
 var material = new THREE.MeshBasicMaterial({color: 0xffff00}); 
-var cube = new THREE.Mesh(geometry, material); 
-scene.add(cube); 
+var floor = new THREE.Mesh(geometry, material); 
+scene.add(floor); 
 camera.position.z = 2; 
+camera.position.y = 0.5; 
 renderer.render(scene, camera);
