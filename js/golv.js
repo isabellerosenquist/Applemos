fe885b01b2c983ccfa51e03390660bef88a566d0
Isabelle Rosenquist <isabelle.rosenquist@gmail.com>
2016-02-16 11:44:45 +0100

    Ändrade käglans storlek

--- js/golv.js
+++ js/golv.js
@@ -21,12 +21,11 @@ var floor = new THREE.Mesh(geometry, material);
 scene.add(floor); 
 
 //KÄGLA
-var geometry2= new THREE.CylinderGeometry( 2, 2, 5, 5 );
+var geometry2= new THREE.CylinderGeometry( 2, 2, 3, 5 );
 var material2 = new THREE.MeshBasicMaterial( {color: 0x0033CC} );
 var cylinder = new THREE.Mesh( geometry2, material2 );
 cylinder.translateZ(-5);
 cylinder.translateY(0);
-
 
 
 scene.add( cylinder );
