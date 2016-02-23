864518ace1d8f4576fe21a0b750b5910ed5fdec3
MaddeRapp <madde.rapp@hotmail.com>
2016-02-23 10:14:10 +0100

    nu är det en funktion

--- js/golv.js
+++ js/golv.js
@@ -1,11 +1,20 @@
-var scene = new THREE.Scene(); 
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000); 
-var renderer = new THREE.WebGLRenderer();
+var scene; 
+var camera; 
+var renderer;
+
+init();
+
+function init(){
+
+scene = new THREE.Scene(); 
+camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 1, 1000); 
+renderer = new THREE.WebGLRenderer();
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement); 
 //var geometry = new THREE.BoxGeometry(1,1,1);
 
-//BANA
+//BANA -------------------------
 var geometry = new THREE.Geometry(); 
 
 geometry.vertices.push(new THREE.Vector3(-0.75, 0, -10));//Vertice 0
@@ -17,22 +26,13 @@ geometry.faces.push(new THREE.Face3( 1, 0, 2));
 geometry.faces.push(new THREE.Face3( 2, 0, 3));
 			
 var material = new THREE.MeshBasicMaterial({color: 0xffff00}); 
+//var material = new THREE.MeshPhongMaterial( { ambient: 0x050505, color: 0x0033ff, specular: 0x555555, shininess: 30 } );
+//var material = new THREE.MeshPhongMaterial( { map: THREE.ImageUtils.loadTexture('/Users/madeleinerapp/Documents/LiU/Githubmappen/Applemos/js/trad.jpg') } );
 var floor = new THREE.Mesh(geometry, material); 
 scene.add(floor); 
 
-//KÄGLA
+//KÄGLA -------------------------
 var geometry2= new THREE.CylinderGeometry( 2, 2, 5, 5 );
-
-// load a texture, set wrap mode to repeat
-var texture = new THREE.TextureLoader().load( "/Users/madeleinerapp/Desktop/fortrollande_skog_large.jpg " );
-texture.wrapS = THREE.RepeatWrapping;
-texture.wrapT = THREE.RepeatWrapping;
-texture.repeat.set( 4, 4 );
-
-
-//var map = new THREE.TextureLoader().load( '/Users/madeleinerapp/Desktop/fortrollande_skog_large.jpg ' );
-//map.wrapS = map.wrapT = THREE.RepeatWrapping;
-//map.anisotropy = 16;
 
 
 var material2 = new THREE.MeshBasicMaterial( {color: 0x0033CC} );
@@ -41,9 +41,73 @@ cylinder.translateZ(-5);
 cylinder.translateY(2);
 
 
-scene.add( cylinder );
+scene.add( cylinder);
+
 
 camera.position.z = 15; 
 camera.position.y = 1.5;
 camera.position.x = 2;
+
+
 renderer.render(scene, camera);
+
+}
+
+/*
+	
+var camera;
+var scene;
+var renderer;
+var mesh;
+  
+init();
+animate();
+  
+function init() {
+  
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000);
+  
+    var light = new THREE.DirectionalLight( 0xffffff );
+    light.position.set( 0, 1, 1 ).normalize();
+    scene.add(light);
+  
+    var geometry = new THREE.CubeGeometry( 10, 10, 10);
+    var material = new THREE.MeshPhongMaterial( { ambient: 0x050505, color: 0x0033ff, specular: 0x555555, shininess: 30 } );
+     
+    // Denna istälelt för den ovan ska generera en bild som textur men bilden vill inte hittas
+	//var material = new THREE.MeshPhongMaterial( { map: THREE.ImageUtils.loadTexture('/Users/madeleinerapp/Documents/LiU/Githubmappen/Applemos/js/images/crate.jpg') } );  
+
+
+    mesh = new THREE.Mesh(geometry, material );
+    mesh.position.z = -50;
+    scene.add( mesh );
+  
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+  
+    window.addEventListener( 'resize', onWindowResize, false );
+  
+    render();
+}
+  
+function animate() {
+    mesh.rotation.x += .04;
+    mesh.rotation.y += .02;
+  
+    render();
+    requestAnimationFrame( animate );
+}
+  
+function render() {
+    renderer.render( scene, camera );
+}
+  
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    render();
+}
+*/
