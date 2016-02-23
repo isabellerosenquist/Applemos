cd7f7ab9b90b712b8ba4f9faa63e11c3b9c328f8
Victoria Waldemarson <vicwa944@student.liu.se>
2016-02-23 10:58:37 +0100

    mess me

---
+++ js/startevent.js
@@ -0,0 +1,15 @@
+<head>
+    <script type="text/javascript">
+        var KLOTstart = null;
+
+        function Init () {
+            var button = document.getElementById ("Bowlingklotets startvärde");
+            
+        }
+    </script>
+</head>
+<body onload="Init ()">
+    Press the following button and release the mouse button somewhere inside or outside the browser.
+    <br />
+    <button id="testButton">Tryck</button>
+</body>
